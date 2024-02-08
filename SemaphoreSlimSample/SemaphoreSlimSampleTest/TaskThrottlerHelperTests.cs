12e51f6ceaf41ff537599fbd890ad5125e39812d
flgaja <kumark1988k7@hotmail.com>
2024-02-07 21:08:40 -0500

    Initial Commit

---
+++ SemaphoreSlimSample/SemaphoreSlimSampleTest/TaskThrottlerHelperTests.cs
@@ -0,0 +1,48 @@
+﻿using Xunit.Abstractions;
+
+namespace SemaphoreSlimSampleTest
+{
+    public class TaskThrottlerHelperTests
+    {
+        private readonly ITestOutputHelper _logger;
+
+        public TaskThrottlerHelperTests(ITestOutputHelper logger)
+        {
+            _logger = logger;
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(3)]
+        [InlineData(10)]
+        [InlineData(15)]
+        [InlineData(100)]
+        [InlineData(150)]
+        public async Task TaskThrottlerHelper_ExecuteWithSemaphoreAsync_DoesNotExceedMaxAllowedConcurrency(int maxAllowedConcurrency)
+        {
+            // Arrance 
+            var sampleList = Enumerable.Range(0, 100).Select(i => i);
+            var throttledTask = new TaskThrottlingHelper<int>(_logger, sampleList, i => Task.Delay(i), maxAllowedConcurrency);
+            
+            // Act
+            await throttledTask.ExecuteWithSemaphoreAsync().ConfigureAwait(false);
+
+            // Assert
+            Assert.True (throttledTask.MaxConcurrencyAcheived <=  maxAllowedConcurrency);
+        }
+
+        [Fact]
+        public async Task TaskThrottlerHelper_ExecuteWithOutSemaphoreAsync()
+        {
+            // Arrance 
+            var sampleList = Enumerable.Range(0, 100).Select(i => i);
+            var throttledTask = new TaskThrottlingHelper<int>(_logger, sampleList, i => Task.Delay(i), 1);
+
+            // Act
+            await throttledTask.ExecuteWithoutSemaphoreAsync().ConfigureAwait(false);
+
+            // Assert
+            Assert.True(throttledTask.MaxConcurrencyAcheived > 1);
+        }
+    }
+}
---
+++ SemaphoreSlimSample/SemaphoreSlimSampleTest/TaskThrottlingHelper.cs
@@ -0,0 +1,102 @@
+﻿using Xunit.Abstractions;
+
+namespace SemaphoreSlimSampleTest
+{
+    internal class TaskThrottlingHelper<T>
+    {
+        private readonly ITestOutputHelper _logger;
+        private readonly int _maxConcurrency = 1;
+
+        private readonly IEnumerable<T> _collectionToLoop;
+        private readonly Func<T, Task> _actionToMoq;
+
+        private int _maxConcurrencyAcheived;
+        private int _numberOfParallelTasks;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="logger">Logger</param>
+        /// <param name="collectionToLoop">Collection to Loop through asynchronously</param>
+        /// <param name="actionToMoq">Action to be performed on each item in the collection</param>
+        /// <param name="maxConcurrency">Number of Tasks that can execute concurrently</param>
+
+        public TaskThrottlingHelper(ITestOutputHelper logger, IEnumerable<T> collectionToLoop, Func<T, Task> actionToMoq, int maxConcurrency)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _collectionToLoop = collectionToLoop ?? throw new ArgumentNullException(nameof(collectionToLoop));
+            _actionToMoq = actionToMoq ?? throw new ArgumentNullException(nameof(actionToMoq));
+            _maxConcurrency = (maxConcurrency == 0) ? throw new ArgumentOutOfRangeException(nameof(maxConcurrency), "Value cannot be zero or less") : maxConcurrency;
+        }
+
+        public int MaxConcurrencyAcheived => _maxConcurrencyAcheived;
+
+        public async Task ExecuteWithSemaphoreAsync()
+        {
+            _maxConcurrencyAcheived = 0;
+            //Initialize a Semaphore Slim to control the number of tasks that can run parallely
+            using (var semaphore = new SemaphoreSlim(_maxConcurrency, _maxConcurrency))
+            {
+                _logger.WriteLine($"{DateTime.Now} - Initialized semaphore with capacity of {semaphore.CurrentCount} tasks");
+                var tasks = _collectionToLoop.Select(async item =>
+                {
+                    _logger.WriteLine($"{DateTime.Now} - Processing Item {item} - available slots in semaphore {semaphore.CurrentCount}");
+
+                    // semaphore wait will block the task until a slot opens up for this task to continue.
+                    await semaphore.WaitAsync().ConfigureAwait(false);
+
+                    // Perform desired action
+                    try
+                    {
+                        // ++ operator & -- operator are not thread safe, Use Interlocked.Increment() instead;
+                        // ++_numberOfParallelTasks; 
+                        Interlocked.Increment(ref _numberOfParallelTasks);
+                        _maxConcurrencyAcheived = _numberOfParallelTasks > _maxConcurrencyAcheived ? _numberOfParallelTasks : _maxConcurrencyAcheived;
+                        _logger.WriteLine($"{DateTime.Now} - Item {item} - Running {_numberOfParallelTasks} in parallel");
+                        await _actionToMoq(item);
+                    }
+                    finally
+                    {
+                        Interlocked.Decrement(ref _numberOfParallelTasks);
+                        semaphore.Release(); // release an occupied slot for next waiting task;
+                    }
+                }).ToArray();
+
+                await Task.WhenAll(tasks).ConfigureAwait(false);
+
+                _logger.WriteLine($"{DateTime.Now} - Max concurrency acheived = {_maxConcurrencyAcheived}");
+
+            }
+        }
+
+        public async Task ExecuteWithoutSemaphoreAsync()
+        {
+            _maxConcurrencyAcheived = 0;
+            var tasks = _collectionToLoop.Select(async item =>
+            {
+                _logger.WriteLine($"{DateTime.Now} - Processing Item {item}");
+
+                // Perform desired action
+                try
+                {
+                    // ++ operator & -- operator are not thread safe, Use Interlocked.Increment() instead;
+                    // ++_numberOfParallelTasks; 
+                    Interlocked.Increment(ref _numberOfParallelTasks);
+                    _maxConcurrencyAcheived = _numberOfParallelTasks > _maxConcurrencyAcheived ? _numberOfParallelTasks : _maxConcurrencyAcheived;
+                    _logger.WriteLine($"{DateTime.Now} - Item {item} - Running {_numberOfParallelTasks} in parallel");
+                    await _actionToMoq(item);
+                }
+                finally
+                {
+                    Interlocked.Decrement(ref _numberOfParallelTasks);
+                }
+            }).ToArray();
+
+            await Task.WhenAll(tasks).ConfigureAwait(false);
+
+            _logger.WriteLine($"{DateTime.Now} - Max concurrency acheived = {_maxConcurrencyAcheived}");
+
+
+        }
+    }
+}
